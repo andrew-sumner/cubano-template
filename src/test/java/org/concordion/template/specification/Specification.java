ddf0c5a04a34f3149c62bb88df4cb5b7f6d3f0a0
Andrew Sumner <andrew.sumner@xtra.co.nz>
2017-11-22 23:19:45 +1300

    Comments

--- src/test/java/org/concordion/template/specification/Specification.java
+++ src/test/java/org/concordion/template/specification/Specification.java
@@ -3,8 +3,6 @@ package org.concordion.template.specification;
 
 /**
  * Run me as a Junit Test to test all specifications.
- * @author jkemp002
- *
  */
 public class Specification extends ConcordionIndex {
 
